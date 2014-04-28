d9af4aec3cc9ab9c27041474dc49e08731c99751
Sebastian Erdweg <seba.github@gmail.com>
2014-04-28 10:03:34 +0200

    fix web site

--- copy-to-server.sh
+++ copy-to-server.sh
@@ -1,4 +1,7 @@
 SERVER=xx00seba@clientssh2.rbg.informatik.tu-darmstadt.de
 DIR=.public_html/projects/sugarj
+SERVER2=tanga.mathematik.uni-marburg.de
+DIR2=public_html/projects/sugarj
 
 rsync -av --exclude=".svn" --exclude=".git" --exclude="*~" --exclude="*.sh" --exclude=".DS_Store" . $SERVER:$DIR
+rsync -av --exclude=".svn" --exclude=".git" --exclude="*~" --exclude="*.sh" --exclude=".DS_Store" . $SERVER2:$DIR2
