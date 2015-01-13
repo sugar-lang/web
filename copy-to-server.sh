d900137c2a3562f60100a71514de9101c9359014
Sebastian Erdweg <seba.github@gmail.com>
2015-01-13 10:59:33 +0100

    update mdd info

--- copy-to-server.sh
+++ copy-to-server.sh
@@ -1,6 +1,6 @@
 SERVER=xx00seba@clientssh2.rbg.informatik.tu-darmstadt.de
 DIR=.public_html/projects/sugarj
-SERVER2=tanga.mathematik.uni-marburg.de
+SERVER2=login.mathematik.uni-marburg.de
 DIR2=public_html/projects/sugarj
 
 rsync -av --exclude=".svn" --exclude=".git" --exclude="*~" --exclude="*.sh" --exclude=".DS_Store" . $SERVER:$DIR
